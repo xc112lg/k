58ff4e5a3a909c9180cd13339fd61afa098a06ee
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-07-09 07:57:22 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -4,12 +4,12 @@
 
 
 sudo apt-get install kmod
-
+cp -r  k/toolchains $HOME/toolchains
 git clone https://github.com/LGE-G5-G6-V20/android_kernel_lge_msm8996_r2 --depth 1 -b swan2000-los21
 
 cd android_kernel_lge_msm8996_r2
 git pull
-cp -r  ../toolchains $HOME/toolchains
+
 sed -i '/read -rp "Build failed. Retry? "/,/*) ABORT "Compilation aborted." ;;/c\    if ! make -C "$RDIR" O=$BDIR -j"$THREADS"; then\n        echo "Build failed. Aborting."\n        exit 1\n    fi' build.sh
 sed -i '/if \[ "$ASK_CLEAN" = "yes" \]; then/,/else/{N;d;}' build.sh
 chmod +x build.sh
