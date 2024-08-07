94ee8f16361a8dc3581973dfe4d1a9863a84657f
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-08-07 16:12:42 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -16,8 +16,8 @@ sed -i '/while ! make -C "$RDIR" O=$BDIR -j"$THREADS"; do/,/done/c\echo "Build f
 sed -i '/if \[ "$ASK_CLEAN" = "yes" \]; then/,/else # Always clean build folder for next build on build_all/d' build.sh
 
 
-sleep 1 && git fetch https://github.com/xc112lg/android_kernel_lge_msm8996_r2.git patch-2
-sleep 1 && git cherry-pick 64331eaa542f52d94789f701325ff9508c4b59dc
+# sleep 1 && git fetch https://github.com/xc112lg/android_kernel_lge_msm8996_r2.git patch-2
+# sleep 1 && git cherry-pick 64331eaa542f52d94789f701325ff9508c4b59dc
 #sed -i 's/select SND_SOC_MSM_HDMI_CODEC_RX if ARCH_MSM8996/select SND_SOC_MSM_HDMI_CODEC_RX/' sound/soc/msm/Kconfig
 
 chmod +x build.sh
