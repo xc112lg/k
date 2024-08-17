4d379ec49db3fb0593838d8ebfab22e2f46ea121
Your Name <you@example.com>
2024-08-17 16:34:38 +0800

    f

--- a.sh
+++ a.sh
@@ -19,7 +19,7 @@ sed -i '/if \[ "$ASK_CLEAN" = "yes" \]; then/,/else # Always clean build folder 
 # sleep 1 && git fetch https://github.com/xc112lg/android_kernel_lge_msm8996_r2.git patch-2
 # sleep 1 && git cherry-pick 64331eaa542f52d94789f701325ff9508c4b59dc
 #sed -i 's/select SND_SOC_MSM_HDMI_CODEC_RX if ARCH_MSM8996/select SND_SOC_MSM_HDMI_CODEC_RX/' sound/soc/msm/Kconfig
-#sed -i '/select SND_SOC_MSM_HDMI_CODEC_RX if ARCH_MSM8996/d' sound/soc/msm/Kconfig
+sed -i '/select SND_SOC_MSM_HDMI_CODEC_RX if ARCH_MSM8996/d' sound/soc/msm/Kconfig
 
 chmod +x build.sh
 chmod +x copy_finished.sh
--- toolchains/arm-eabi/lib/gcc/arm-eabi/14.0.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../gcc/configure --target=arm-eabi --disable-decimal-float --disable-docs --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --enable-default-ssp --enable-languages=c,c++,fortran --enable-threads=posix --prefix=/__w/gcc-build/gcc-build/../gcc-arm --with-gnu-as --with-gnu-ld --with-headers=/usr/include --with-linker-hash-style=gnu --with-newlib --with-pkgversion='Eva GCC' --with-sysroot";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "arm7tdmi" } };
