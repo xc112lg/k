95abe9b0c47785fff67350e790f3b39b734c0cca
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-08-17 19:36:32 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -100,26 +100,53 @@ else
 fi
 echo $PWD
 echo "All steps completed successfully."
-# cp -r  k/toolchains $HOME/toolchains
-# git clone https://github.com/xc112lg/android_kernel_lge_msm8996_r2 --depth 1 -b patch-5
-
-# cd android_kernel_lge_msm8996_r2
-# git pull
-
-# #sed -i '/while ! make -C "$RDIR" O=$BDIR -j"$THREADS"; do/,/done/c\echo "Build failed. Aborting."\nexit 1\n' build.sh
+cp -r  k/toolchains $HOME/toolchains
 
 
-# #sed -i '/if \[ "$ASK_CLEAN" = "yes" \]; then/,/else # Always clean build folder for next build on build_all/d' build.s##h
+export GH_TOKEN=$(cat ../gh_token.txt)
+
+if ! command -v gh &> /dev/null; then
+    echo "GitHub CLI 'gh' not found. Downloading and installing..."
+    wget https://github.com/cli/cli/releases/download/v2.40.1/gh_2.40.1_linux_amd64.tar.gz
+    tar -xvf gh_2.40.1_linux_amd64.tar.gz
+    sudo mv gh_*_linux_amd64/bin/gh /usr/local/bin/
+    echo "GitHub CLI 'gh' installed successfully."
+else
+    echo "GitHub CLI 'gh' is already installed."
+fi
 
 
-# # sleep 1 && git fetch https://github.com/xc112lg/android_kernel_lge_msm8996_r2.git patch-2
-# # sleep 1 && git cherry-pick 64331eaa542f52d94789f701325ff9508c4b59dc
-# #sed -i 's/select SND_SOC_MSM_HDMI_CODEC_RX if ARCH_MSM8996/select SND_SOC_MSM_HDMI_CODEC_RX/' sound/soc/msm/Kconfig
-# sed -i '/select SND_SOC_MSM_HDMI_CODEC_RX if ARCH_MSM8996/d' sound/soc/msm/Kconfig
 
-# chmod +x build.sh
-# chmod +x copy_finished.sh
-# ./build.sh H872
-# ./copy_finished.sh
+
+
+
+
+
+
+
+git clone https://github.com/xc112lg/android_kernel_lge_msm8996_r2 --depth 1 -b patch-5
+
+cd android_kernel_lge_msm8996_r2
+git pull
+
+#sed -i '/while ! make -C "$RDIR" O=$BDIR -j"$THREADS"; do/,/done/c\echo "Build failed. Aborting."\nexit 1\n' build.sh
+
+
+#sed -i '/if \[ "$ASK_CLEAN" = "yes" \]; then/,/else # Always clean build folder for next build on build_all/d' build.s##h
+
+
+# sleep 1 && git fetch https://github.com/xc112lg/android_kernel_lge_msm8996_r2.git patch-2
+# sleep 1 && git cherry-pick 64331eaa542f52d94789f701325ff9508c4b59dc
+#sed -i 's/select SND_SOC_MSM_HDMI_CODEC_RX if ARCH_MSM8996/select SND_SOC_MSM_HDMI_CODEC_RX/' sound/soc/msm/Kconfig
+sed -i '/select SND_SOC_MSM_HDMI_CODEC_RX if ARCH_MSM8996/d' sound/soc/msm/Kconfig
+
+chmod +x build.sh
+chmod +x copy_finished.sh
+./build.sh H872
+
+ls
+./copy_finished.sh
+
+
 
 
