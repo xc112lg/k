cd053951db0b9d997d4c6fe3b098105850dd112f
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-08-18 07:54:03 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,68 +1,68 @@
 #!/bin/bash
 
 echo $PWD
-# source  k/fix.sh
-# cp -r  k/toolchains $HOME/toolchains
+source  k/fix.sh
+cp -r  k/toolchains $HOME/toolchains
 
 
 
-# if ! command -v gh &> /dev/null; then
-#     echo "GitHub CLI 'gh' not found. Downloading and installing..."
-#     wget https://github.com/cli/cli/releases/download/v2.40.1/gh_2.40.1_linux_amd64.tar.gz
-#     tar -xvf gh_2.40.1_linux_amd64.tar.gz
-#     sudo mv gh_*_linux_amd64/bin/gh /usr/local/bin/
-#     echo "GitHub CLI 'gh' installed successfully."
-# else
-#     echo "GitHub CLI 'gh' is already installed."
-# fi
+if ! command -v gh &> /dev/null; then
+    echo "GitHub CLI 'gh' not found. Downloading and installing..."
+    wget https://github.com/cli/cli/releases/download/v2.40.1/gh_2.40.1_linux_amd64.tar.gz
+    tar -xvf gh_2.40.1_linux_amd64.tar.gz
+    sudo mv gh_*_linux_amd64/bin/gh /usr/local/bin/
+    echo "GitHub CLI 'gh' installed successfully."
+else
+    echo "GitHub CLI 'gh' is already installed."
+fi
 
 
-# export GH_TOKEN=$(cat gh_token.txt)
-
-
-
-# if ! gh auth status &> /dev/null; then
-#     # User not authenticated, perform login
-#     gh auth login --with-token $GH_TOKEN
-# else
-#     echo "Already authenticated with GitHub."
-# fi
+export GH_TOKEN=$(cat gh_token.txt)
 
 
 
-# rm -rf Evolution-X
-# git clone https://$GH_TOKEN@github.com/xc112lg/Evolution-X.git
-
-# rm -rf android_kernel_lge_msm8996_r2
-# git clone https://github.com/xc112lg/android_kernel_lge_msm8996_r2 --depth 1 -b patch-6
-
-# cd android_kernel_lge_msm8996_r2
-# git pull
-
-# #sed -i '/while ! make -C "$RDIR" O=$BDIR -j"$THREADS"; do/,/done/c\echo "Build failed. Aborting."\nexit 1\n' build.sh
+if ! gh auth status &> /dev/null; then
+    # User not authenticated, perform login
+    gh auth login --with-token $GH_TOKEN
+else
+    echo "Already authenticated with GitHub."
+fi
 
 
-# #sed -i '/if \[ "$ASK_CLEAN" = "yes" \]; then/,/else # Always clean build folder for next build on build_all/d' build.s##h
+
+rm -rf Evolution-X
+git clone https://$GH_TOKEN@github.com/xc112lg/Evolution-X.git
+
+rm -rf android_kernel_lge_msm8996_r2
+git clone https://github.com/xc112lg/android_kernel_lge_msm8996_r2 --depth 1 -b patch-6
+
+cd android_kernel_lge_msm8996_r2
+git pull
+
+#sed -i '/while ! make -C "$RDIR" O=$BDIR -j"$THREADS"; do/,/done/c\echo "Build failed. Aborting."\nexit 1\n' build.sh
 
 
-# # sleep 1 && git fetch https://github.com/xc112lg/android_kernel_lge_msm8996_r2.git patch-2
-# # sleep 1 && git cherry-pick 64331eaa542f52d94789f701325ff9508c4b59dc
-# #sed -i 's/select SND_SOC_MSM_HDMI_CODEC_RX if ARCH_MSM8996/select SND_SOC_MSM_HDMI_CODEC_RX/' sound/soc/msm/Kconfig
-# sed -i '/select SND_SOC_MSM_HDMI_CODEC_RX if ARCH_MSM8996/d' sound/soc/msm/Kconfig
-
-# chmod +x build.sh
-# chmod +x copy_finished.sh
-# ./build.sh H872
-
-# ls
-# ./copy_finished.sh
-# mv out/*.zip ../Evolution-X
-# cd .. 
+#sed -i '/if \[ "$ASK_CLEAN" = "yes" \]; then/,/else # Always clean build folder for next build on build_all/d' build.s##h
 
 
-# cd Evolution-X
-# chmod u+x multi_upload.sh
-# . multi_upload.sh
+# sleep 1 && git fetch https://github.com/xc112lg/android_kernel_lge_msm8996_r2.git patch-2
+# sleep 1 && git cherry-pick 64331eaa542f52d94789f701325ff9508c4b59dc
+#sed -i 's/select SND_SOC_MSM_HDMI_CODEC_RX if ARCH_MSM8996/select SND_SOC_MSM_HDMI_CODEC_RX/' sound/soc/msm/Kconfig
+sed -i '/select SND_SOC_MSM_HDMI_CODEC_RX if ARCH_MSM8996/d' sound/soc/msm/Kconfig
+
+chmod +x build.sh
+chmod +x copy_finished.sh
+./build.sh H872
+
+ls
+./copy_finished.sh
+mv out/*.zip ../Evolution-X
+cd .. 
+
+
+cd Evolution-X
+chmod u+x multi_upload.sh
+. multi_upload.sh
 
 
 
