679b6782e02e602824d04eaecf8afb5986f9ceb1
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-08-19 16:43:25 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 
+
+
 source  k/fix.sh
 cp -r  k/toolchains $HOME/toolchains
 
@@ -31,32 +33,66 @@ fi
 
 
 #rm -rf Evolution-X
-#git clone https://$GH_TOKEN@github.com/xc112lg/Evolution-X.git
-
+git clone https://$GH_TOKEN@github.com/xc112lg/Evolution-X.git
 rm Evolution-X/*.zip
 rm -rf android_kernel_lge_msm8996_r2
 git clone https://github.com/xc112lg/android_kernel_lge_msm8996_r2 --depth 1 -b patch-7
 
-#git clone https://github.com/LGE-G5-G6-V20/android_kernel_lge_msm8996_r2  --depth 1 -b swan2000-los21
-
 cd android_kernel_lge_msm8996_r2
-#rm arch/arm64/configs/lge_msm8996_defconfig
-#wget -N -P arch/arm64/configs/ https://raw.githubusercontent.com/xc112lg/android_kernel_lge_msm8996_r2/patch-6/arch/arm64/configs/lge_msm8996_defconfig
+rm arch/arm64/configs/vendor/lge/lge_msm8996_defconfig
+#wget -N -P arch/arm64/configs/vendor/lge https://raw.githubusercontent.com/xc112lg/android_kernel_lge_msm8996_r2/patch-7/arch/arm64/configs/vendor/lge/lge_msm8996_defconfig
 #git pull origin patch-6
 
 #sed -i '/while ! make -C "$RDIR" O=$BDIR -j"$THREADS"; do/,/done/c\echo "Build failed. Aborting."\nexit 1\n' build.sh
 
-sed -i '/while ! make -C "$RDIR" O=$BDIR -j"$THREADS"; do/,/done/c\while ! make -C "$RDIR" O=$BDIR -j"$THREADS"; do\n    ABORT "Compilation aborted."\ndone' build.sh
 
-#sed -i '/read -rp "Build failed. Retry? "/,+3c\ABORT "Compilation aborted."' build.sh
+#sed -i '/if \[ "$ASK_CLEAN" = "yes" \]; then/,/else # Always clean build folder for next build on build_all/d' build.s##h
 
-sed -i '/if \[ "$ASK_CLEAN" = "yes" \]; then/,/else # Always clean build folder for next build on build_all/d' build.sh
-
+#sed -i '/if \[ "$ASK_CLEAN" = "yes" \]; then/{N;s/if \[ "$ASK_CLEAN" = "yes" \]; then\n *while true; do.*/if [ "$ASK_CLEAN" = "no" ]; then\n    echo "Skipping clean. Not deleting build directory."\nelse\n    CLEAN_BUILD\nfi/}' build.sh
 
 # sleep 1 && git fetch https://github.com/xc112lg/android_kernel_lge_msm8996_r2.git patch-2
 # sleep 1 && git cherry-pick 64331eaa542f52d94789f701325ff9508c4b59dc
 #sed -i 's/select SND_SOC_MSM_HDMI_CODEC_RX if ARCH_MSM8996/select SND_SOC_MSM_HDMI_CODEC_RX/' sound/soc/msm/Kconfig
 # sed -i '/select SND_SOC_MSM_HDMI_CODEC_RX if ARCH_MSM8996/d' sound/soc/msm/Kconfig
+rm -rf build.sh
+
+# Fetch the latest commit hash and print it for debugging
+LATEST_COMMIT=$(curl -s "https://api.github.com/repos/xc112lg/android_kernel_lge_msm8996_r2/commits?path=build.sh&sha=patch-7" | jq -r '.[0].sha')
+echo "Latest Commit: $LATEST_COMMIT"
+
+# Check if the commit hash was fetched correctly
+if [ -n "$LATEST_COMMIT" ]; then
+  # Construct the URL and print it
+  URL="https://raw.githubusercontent.com/xc112lg/android_kernel_lge_msm8996_r2/$LATEST_COMMIT/build.sh"
+  echo "Downloading from URL: $URL"
+
+  # Download the commands.txt file using the latest commit hash
+  wget "$URL" -O build.sh
+else
+  echo "Failed to retrieve the latest commit hash."
+fi
+
+
+
+
+
+
+
+LATEST_COMMIT1=$(curl -s "https://api.github.com/repos/xc112lg/android_kernel_lge_msm8996_r2/commits?path=arch/arm64/configs/vendor/lge/lge_msm8996_defconfig&sha=patch-7" | jq -r '.[0].sha')
+echo "Latest Commit: $LATEST_COMMIT"
+
+# Check if the commit hash was fetched correctly
+if [ -n "$LATEST_COMMIT1" ]; then
+  # Construct the URL and print it
+  URL1="https://raw.githubusercontent.com/xc112lg/android_kernel_lge_msm8996_r2/$LATEST_COMMIT/arch/arm64/configs/vendor/lge/lge_msm8996_defconfig"
+  echo "Downloading from URL: $URL"
+
+  # Download the commands.txt file using the latest commit hash
+  wget "$URL1" -O arch/arm64/configs/vendor/lge/lge_msm8996_defconfig
+else
+  echo "Failed to retrieve the latest commit hash."
+fi
+
 
 chmod +x build.sh
 chmod +x copy_finished.sh
@@ -78,6 +114,8 @@ fi
 cd Evolution-X
 chmod u+x multi_upload.sh
 . multi_upload.sh
+
+cd ..
 
 
 
