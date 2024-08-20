1ce8560adcd024839b0920a8a6cd38e3b01fca50
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-08-20 21:39:00 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -39,7 +39,7 @@ rm -rf android_kernel_lge_msm8996_r2
 git clone https://github.com/xc112lg/android_kernel_lge_msm8996_r2 --depth 1 -b patch-7
 
 cd android_kernel_lge_msm8996_r2
-rm arch/arm64/configs/vendor/lge/lge_msm8996_defconfig
+#rm arch/arm64/configs/vendor/lge/lge_msm8996_defconfig
 #wget -N -P arch/arm64/configs/vendor/lge https://raw.githubusercontent.com/xc112lg/android_kernel_lge_msm8996_r2/patch-7/arch/arm64/configs/vendor/lge/lge_msm8996_defconfig
 #git pull origin patch-6
 
@@ -54,44 +54,7 @@ rm arch/arm64/configs/vendor/lge/lge_msm8996_defconfig
 # sleep 1 && git cherry-pick 64331eaa542f52d94789f701325ff9508c4b59dc
 #sed -i 's/select SND_SOC_MSM_HDMI_CODEC_RX if ARCH_MSM8996/select SND_SOC_MSM_HDMI_CODEC_RX/' sound/soc/msm/Kconfig
 # sed -i '/select SND_SOC_MSM_HDMI_CODEC_RX if ARCH_MSM8996/d' sound/soc/msm/Kconfig
-rm -rf build.sh
 
-# Fetch the latest commit hash and print it for debugging
-LATEST_COMMIT=$(curl -s "https://api.github.com/repos/xc112lg/android_kernel_lge_msm8996_r2/commits?path=build.sh&sha=patch-7" | jq -r '.[0].sha')
-echo "Latest Commit: $LATEST_COMMIT"
-
-# Check if the commit hash was fetched correctly
-if [ -n "$LATEST_COMMIT" ]; then
-  # Construct the URL and print it
-  URL="https://raw.githubusercontent.com/xc112lg/android_kernel_lge_msm8996_r2/$LATEST_COMMIT/build.sh"
-  echo "Downloading from URL: $URL"
-
-  # Download the commands.txt file using the latest commit hash
-  wget "$URL" -O build.sh
-else
-  echo "Failed to retrieve the latest commit hash."
-fi
-
-
-
-
-
-
-
-LATEST_COMMIT1=$(curl -s "https://api.github.com/repos/xc112lg/android_kernel_lge_msm8996_r2/commits?path=arch/arm64/configs/vendor/lge/lge_msm8996_defconfig&sha=patch-7" | jq -r '.[0].sha')
-echo "Latest Commit: $LATEST_COMMIT"
-
-# Check if the commit hash was fetched correctly
-if [ -n "$LATEST_COMMIT1" ]; then
-  # Construct the URL and print it
-  URL1="https://raw.githubusercontent.com/xc112lg/android_kernel_lge_msm8996_r2/$LATEST_COMMIT/arch/arm64/configs/vendor/lge/lge_msm8996_defconfig"
-  echo "Downloading from URL: $URL"
-
-  # Download the commands.txt file using the latest commit hash
-  wget "$URL1" -O arch/arm64/configs/vendor/lge/lge_msm8996_defconfig
-else
-  echo "Failed to retrieve the latest commit hash."
-fi
 
 
 chmod +x build.sh
